55a0e363f4ceabbefc7601d8d0a8f0666dd77103
judomasaya <ngudct.hayamasa@gmail.com>
2020-04-02 17:55:18 +0900

    view change

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
   root to: 'posts#index'#/にアクセスしたらこれでindex画面に飛ばす
-  resources :posts
+  resources :posts#下のをリファクタリングする、postsだけで良くなる
   # only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users, only: :show #usersコントローラーを作成　詳細見るやーつrails g controller users 忘れずに
+  resources :users, only: :show #usersコントローラーを作成しこれを記述
+  # 　マイページの詳細を見るやーつrails g controller users 忘れずに
+  # これで、/users/:idのパスでアクセスした際にusers_controller.rbの
+  # showアクションを呼ぶルーティングが設定できました。
 end
 
 # resources :posts, except: :index
