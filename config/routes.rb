1499092c086bb987c89771c319caa7e9d0c96f8b
judomasaya <ngudct.hayamasa@gmail.com>
2020-06-29 08:43:46 +0900

    view調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,24 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   devise_for :installs
-  root to: 'posts#index'#/にアクセスしたらこれでindex画面に飛ばす
-
-
-  resources :posts do#下のをリファクタリングする、postsだけで良くなる
+  root to: 'posts#index' #/にアクセスしたらこれでindex画面に飛ばす
+  resources :posts do#下のをリファクタリングすると、postsだけで良くなる
   # only: [:index, :new, :create, :show, :edit, :update, :destroy]
-    resources :comments, only: :create
-        collection do
-      get 'search'
-    end
-
+    resources :comments, only: [:create]
+      collection do
+        get 'search'
+      end
   end
   # ルーティングをネストさせる一番の理由はアソシエーション先のレコードのidを
   # paramsに追加してコントローラーに送るためです。
   # 今回の実装だと、コメントと結びつく投稿（ぽすと）のidをparamsに追加しています。
 
-
-
   resources :users, only: :show #usersコントローラーを作成しこれを記述
+end
   # 　マイページの詳細を見るやーつrails g controller users 忘れずに
   # これで、/users/:idのパスでアクセスした際にusers_controller.rbの
   # showアクションを呼ぶルーティングが設定できました。
-end
 
 # resources :posts, except: :index
 # exceptを使用するとindex以外は網羅したぜ？という意味
